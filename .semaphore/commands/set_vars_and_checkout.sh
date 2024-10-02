2fc5427e93cfd5616699199efcc6314e81787f31
Adam Bennett <adam@hozah.com>
2024-10-02 16:34:27 +0100

    Bumps
    Took 1 minute

--- .semaphore/commands/set_vars_and_checkout.sh
+++ .semaphore/commands/set_vars_and_checkout.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export REPO_NAME='enforcement'
-export MAIN_BRANCH='master'
+export REPO_NAME='kestrel'
+export MAIN_BRANCH='0.3.9_hozah'
 
 export BRANCH_NAME="${SEMAPHORE_GIT_WORKING_BRANCH:-"${MAIN_BRANCH}"}"
 
